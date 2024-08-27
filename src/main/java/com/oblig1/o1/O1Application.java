7177436fbc0233121096d22ab900ebb5d90d0cc0
h598999 <h598999@stud.hvl.no>
2024-08-27 17:36:01 +0200

    lol

---
+++ src/main/java/com/oblig1/o1/O1Application.java
@@ -0,0 +1,13 @@
+package com.oblig1.o1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/oblig1/o1/O1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oblig1.o1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
