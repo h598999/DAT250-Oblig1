649b275c9e5d50575d5afc6328b0e3e20280aff8
h598999 <h598999@stud.hvl.no>
2024-08-29 21:26:07 +0200

    Gradle!

--- src/main/java/com/oblig1/o1/O1Application.java
+++ src/main/java/com/oblig1/o1/O1Application.java
@@ -2,12 +2,22 @@ package com.oblig1.o1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class O1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(O1Application.class, args);
 	}
+
+  @GetMapping
+  public String getIndex(){
+    return "Hello World!";
+  }
 
 }
