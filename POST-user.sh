740609e03198aa0d82d4f9f3f4db4b1ec405c08c
h598999 <h598999@stud.hvl.no>
2024-09-06 08:12:17 +0200

    Removed some!

--- POST-user.sh
+++
@@ -1,7 +0,0 @@
-curl -X POST http://localhost:8080/api/v2/users \
--H "Content-Type: application/json" \
--d '{
-  "username": "john_doe",
-  "email": "john@example.com"
-}' | jq
-
--- test-polls.sh
+++
@@ -1,2 +0,0 @@
-echo "GET /polls \n"
-curl -X GET localhost:8080/api/v2/polls | jq
